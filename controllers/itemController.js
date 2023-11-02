635864c5d8522669c858bbfb33a65b8bc6b2bfda
lc <gingeraleisthebest@gmail.com>
2023-11-01 22:52:56 -0400

    implemented create item form

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -47,29 +47,143 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
   const rarityColl = mongoClient.db('inventory_info').collection('rarity');
   const typeColl = mongoClient.db('inventory_info').collection('type');
   const classColl = mongoClient.db('inventory_info').collection('class');
+  const itemColl = mongoClient.db('inventory_info').collection('item');
   
-  const [allRarity, allType, allClass] = await Promise.all([
+  const [allRarity, allType, allClass, allItem] = await Promise.all([
     rarityColl.find({}, { populate: { _id: 0 } }).toArray(),
     typeColl.find({}, { populate: { _id: 0 } }).toArray(),
     classColl.find({}, { populate: { _id: 0 } }).toArray(),
+    itemColl.find({}, { populate: { _id: 0, name: 1 } }).toArray(),
   ]);
 
   const formattedRarity = allRarity.map(item => capitalizeFirstLetter(item.name));
-  const formattedType = allType.map(item => capitalizeFirstLetter(item.name));
+  const formattedType = allType
+    .map(item => capitalizeFirstLetter(item.name))
+    .sort((a, b) => {
+      return (a.toUpperCase() < b.toUpperCase()) ? -1 :
+      (a.toUpperCase() > b.toUpperCase()) ? 1 : 0;
+    });
   const formattedClass = allClass.map(item => capitalizeFirstLetter(item.name));
+  const sortedItems = allItem.sort((a, b) => {
+    return (a.name.toUpperCase() < b.name.toUpperCase()) ? -1 :
+    (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : 0;
+  });
 
   res.render('item_form', { 
     title: 'Create new item',
     allRarity: formattedRarity,
     allType: formattedType,
     allClass: formattedClass,
+    allItem: sortedItems,
   });
 });
 
 // Handle item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item create POST");
-});
+exports.item_create_post = [
+  body('name', 'name is required')
+    .trim()
+    .isString()
+    .custom(async(value) => {
+      const itemColl = mongoClient.db('inventory_info').collection('item');
+      const itemExists = await itemColl.findOne({ name: value });
+      if (itemExists) {
+        throw new Error('Item already exists');
+      } else {
+        return true;
+      }
+    })
+    .notEmpty()
+    .escape(),
+  body('effect', 'effect is required')
+    .trim()
+    .isString()
+    .notEmpty()
+    .escape(),
+  body('rarity', 'rarity is required')
+    .trim()
+    .isString()
+    .notEmpty()
+    .escape(),
+  body('type', 'type is required')
+    .custom((value) => {
+      const isString = typeof value === 'string';
+      const isArray = Array.isArray(value)
+      if ( !isString && !isArray) {
+        throw new Error(`invalid type. isString=${isString}, isArray=${isArray}`)
+      };
+      return true;
+    })
+    .escape(),
+  body('cost', 'cost is required')
+    .trim()
+    .isNumeric()
+    .notEmpty()
+    .escape(),
+  body('class', 'class error')
+    .trim()
+    .optional()
+    .escape(),
+  body('requires', 'requires error')
+    .trim()
+    .optional()
+    .escape(),
+  body('used_in_recipe', 'used_in_recipes error')
+    .trim()
+    .optional()
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const rarityColl = mongoClient.db('inventory_info').collection('rarity');
+    const typeColl = mongoClient.db('inventory_info').collection('type');
+    const classColl = mongoClient.db('inventory_info').collection('class');
+    const itemColl = mongoClient.db('inventory_info').collection('item');
+    const { name, effect, rarity, type, cost, of_class, requires, used_in_recipe, } = req.body;
+    const newItem = {
+      name,
+      effect,
+      rarity: rarity.toLowerCase(),
+      type: (Array.isArray(type)) ?  type.map(x => x.toLowerCase()) : new Array(type).map(x.toLowerCase),
+      cost,
+      ...(of_class) && {of_class: (Array.isArray(of_class)) ?  of_class.map(x => x.toLowerCase()) : new Array(of_class).map(x => x.toLowerCase())},
+      ...(requires) && {requires: (Array.isArray(requires)) ?  requires.map(x => x.toLowerCase()) : new Array(requires).map(x => x.toLowerCase())},
+      ...(used_in_recipe) && {used_in_recipe: (Array.isArray(used_in_recipe)) ?  used_in_recipe.map(x => x.toLowerCase()) : new Array(used_in_recipe).map(x => x.toLowerCase())},
+    };
+    if (!errors.isEmpty()) {
+      
+      const [allRarity, allType, allClass, allItem] = await Promise.all([
+        rarityColl.find({}, { populate: { _id: 0 } }).toArray(),
+        typeColl.find({}, { populate: { _id: 0 } }).toArray(),
+        classColl.find({}, { populate: { _id: 0 } }).toArray(),
+        itemColl.find({}, { populate: { _id: 0, name: 1 } }).toArray(),
+      ]);
+    
+      const formattedRarity = allRarity.map(item => capitalizeFirstLetter(item.name));
+      const formattedType = allType
+        .map(item => capitalizeFirstLetter(item.name))
+        .sort((a, b) => {
+          return (a.toUpperCase() < b.toUpperCase()) ? -1 :
+          (a.toUpperCase() > b.toUpperCase()) ? 1 : 0;
+        });
+      const formattedClass = allClass.map(item => capitalizeFirstLetter(item.name));
+      const sortedItems = allItem.sort((a, b) => {
+        return (a.name.toUpperCase() < b.name.toUpperCase()) ? -1 :
+        (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : 0;
+      });
+    
+      res.render('item_form', { 
+        title: 'Create new item',
+        allRarity: formattedRarity,
+        allType: formattedType,
+        allClass: formattedClass,
+        allItem: sortedItems,
+        item: newItem,
+        errors: errors.array(),
+      });
+    } else {
+      const insertedItem = await itemColl.insertOne(newItem);
+      res.redirect(`/catalog/item/${insertedItem.insertedId}`);
+    }
+})];
 
 // Display item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
